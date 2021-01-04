a19517e714747286e5029d271f495f86f306080a
MacMuffin <halfscale3@gmail.com>
2021-01-04 23:08:58 +0800

    Implementation of DAO and Service layer

---
+++ src/main/java/com/springboot/hospital/service/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package com.springboot.hospital.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot.hospital.dao.UserDAO;
+import com.springboot.hospital.model.User;
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+	private UserDAO userDAO;
+	
+	@Autowired
+	public UserServiceImpl(UserDAO theUserDAO) {
+		userDAO = theUserDAO;
+	}
+
+	@Override
+	@Transactional
+	public List<User> findAll() {
+		return userDAO.findAll();
+	}
+
+	@Override
+	@Transactional
+	public User findById(int id) {
+		return userDAO.findById(id);
+	}
+
+	@Override
+	@Transactional
+	public void save(User user) {
+		userDAO.save(user);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int id) {
+		userDAO.deleteById(id);
+	}
+
+}
